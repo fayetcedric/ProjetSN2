9d72311ec8b50bbf84fb9476b221ee05304c735c
fayetcedric <compte@cfayet.eu>
2013-02-05 17:37:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include "contante.h"
 #include "global.h"
 
+/* Constantes globales : */
+int flag_DemandeUtilisateur;
+
+int value_Temperature; // Température est comprise entre -55° et 150°
+int value_VitesseVent;
+int value_Pluviometrie;
+
+
 void main(void)
 {
 	/* Boucle de travail : */
 	while(1)
 	{
-		
+		if(flag_DemandeUtilisateur)
 	}
 }
