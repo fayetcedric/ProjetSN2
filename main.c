69f2abe68f5913b07b5bfdb7bb4ff3e2eebe1829
fayetcedric <compte@cfayet.eu>
2013-02-05 18:13:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,39 @@
 #include "global.h"
 
 /* Constantes globales : */
-int flag_DemandeUtilisateur;
+int flag_DemandeUtilisateur = 0;
+int flag_DemandeTemperature = 0;
+int flag_DemandeVitesseVent = 0;
+int flag_DemandePluviometrie = 0;
 
 int value_Temperature; // Température est comprise entre -55° et 150°
 int value_VitesseVent;
 int value_Pluviometrie;
-
 
 void main(void)
 {
 	/* Boucle de travail : */
 	while(1)
 	{
-		if(flag_DemandeUtilisateur)
+		if(flag_Pluviometrie)
+		{
+			
+		}
+		if(flag_DemandeVitesseVent)
+		{
+			
+		}
+		
+		if(flag_DemandeTemperature)
+		{
+			
+		}
+		
+		if(flag_DemandeUtilisateur) // Evenement extérieur au systeme prioritaire
+		{
+			
+		}
 	}
 }
+
+// fAIRE 
