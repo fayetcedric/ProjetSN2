cf93f79f890810dd7c4bfb25c0f7f17d2295aa5e
fayetcedric <compte@cfayet.eu>
2013-02-11 16:15:43 +0100

    Update global.h

--- global.h
+++ global.h
@@ -11,11 +11,6 @@ Fichier contenant les variables et fonctions globales
 /* Fonctions globales : */
 
 /* Constantes globales : */
-extern int flag_DemandeUtilisateur;
-extern int flag_DemandeTemperature;
-extern int flag_DemandeVitesseVent;
-extern int flag_DemandePluviometrie;
-
 extern int value_Temperature; // Température est comprise entre -55° et 150°
 extern int value_VitesseVent;
 extern int value_Pluviometrie;
