0eade94988da3a61b589568c763317656491c1f5
fayetcedric <compte@cfayet.eu>
2013-02-11 14:54:51 +0100

    Update lib_main.c

--- lib_main.c
+++ lib_main.c
@@ -1,1 +1,19 @@
-p
+
+void  fct_init_VariablesGlobales()
+{
+  extern int flag_DemandeUtilisateur = 0;
+  
+  //Initialement on effectue toutes les mesures.
+  extern int flag_DemandeTemperature = 1;
+  extern int flag_DemandeVitesseVent = 1;
+  extern int flag_DemandePluviometrie = 1;
+
+  //Initialement on affecte toutes les variables globales à zéro.
+  extern int value_Temperature=0;
+  extern int value_VitesseVent=0;
+  extern int value_Pluviometrie=0;
+  
+  extern int value_Heures=0;
+  extern int value_Minutes=0;
+  extern int value_Secondes=0;
+}
