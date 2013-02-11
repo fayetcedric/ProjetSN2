06ea84f1cb68d4f21b260c5b2ae6f889d99b5212
MB22 <matthieu.biache@gmail.com>
2013-02-11 09:06:54 -0800

    Create usart1.c

---
+++ usart1.c
@@ -0,0 +1,25 @@
+/*
+usart1.c
+Crée par Matthieu Biache et Quentin Viotto le 11 février 2013
+Derniere modification: 11 février 2013
+*/
+
+#include "usart1.h"
+
+//Initialisation et démarrage de l'USART
+
+void fct_init_USART1(void)
+{
+  US1_CR = US_RSTTX
+          |US_RXDIS
+          |US_TXEN;
+  US1_MR = US_CHRL_8
+          |US_PAR_NO
+          |US_NBSTOP_1
+          |US_CHMODE_NORMAL;
+}
+
+void fct_start_USART1(void)
+{
+}
+
