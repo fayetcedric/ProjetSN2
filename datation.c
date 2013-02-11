110e8d51baed008ac2c503b5c28020f51dc28018
MB22 <matthieu.biache@gmail.com>
2013-02-11 15:02:47 +0100

    Update datation.c

--- datation.c
+++ datation.c
@@ -1,18 +1,31 @@
-#include <91M55800.H>
+#include "datation.h"
+
 
 //Initialisation et démarrage du RTC
 
-void init_RTC(void)
+void fct_init_RTC(void)
 {
-  RTC_MR = 11;
-  RTC_HMR = 0;
-  RTC_TIMR = 
+  int value_DH;  //Variable locale pour les dizaines d'heures
+  int value_UH;	//Variable locale pour les unités d'heures
+  int value_DM;	//Variable locale pour les dizaines de minutes
+  int value_UM;	//Variable locale pour les unités de minutes
+  int value_DS;	//Variable locale pour les dizaines de secondes
+  int value_US;	//Variable locale pour les unités de secondes
 
-
+  RTC_MR = (1<<0);
+  RTC_HMR = (0<<0);
+  RTC_TIMR = (0<<22)|(value_DH<<20)|(value_UH<<16)|(value_DM<<12)|(value_UM<<8)|(value_DS<<4)|(value_US<<0);
 }
 
 
-void start_RTC(void)
+void fct_start_RTC(void)
 {
-  RTC_MR = 10;
+  RTC_MR = (0<<0);
+}
+
+void fct_read_RTC(void)
+{
+  value_Heures=(RTC_TIMR)*10 + RTC_TIMR
+  value_Minutes=
+  value_Secondes=
 }
