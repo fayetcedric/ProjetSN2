e9ca61d2f4f756e5a99982c97b3f1cd1aa674226
fayetcedric <compte@cfayet.eu>
2013-02-11 17:55:03 +0100

    Update lib_main.c

--- lib_main.c
+++ lib_main.c
@@ -24,18 +24,31 @@ void fct_init_TimerDuMain()
 {
     APMC_PCER=(1<<TC0_ID);
     TC0_RC=TIME_PERIOD_TIMERDUMAIN;
-    TC0_CMR=TIME_PERIOD_TIMERDUMAIN|TC_WAVE|TC_CPCTRG;
+    
+    TC0_CMR=TIME_PERIOD_TIMERDUMAIN|
+            TC_WAVE|
+            TC_CPCTRG;
+            
     TC0_IER=TC_CPCS;
     
 }
 
 void fct_init_AIC()
 {
-  AIC_SMR6=AIC_PRIOR|AIC_SRCTYPE_INT_EDGE_TRIGGER;
+  AIC_SMR6=AIC_PRIOR|
+           AIC_SRCTYPE_INT_EDGE_TRIGGER;
+           
   AIC_SVR6=(int)fct_irq_IncremCptTimer0;
   AIC_IECR=(1<<TC0_ID);
 }
 
-void fct_init_TimerDuMain();
-void fct_start_TimerDuMain();
-void fct_irq_IncremCptTimer0();
+void fct_start_TimerDuMain()
+{
+  TC0_CCR=TC_CLKEN|
+          TC_SWTRG;
+}
+
+void fct_irq_IncremCptTimer0()
+{
+  
+}
