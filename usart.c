31aa5128141c8b283b486a2221bf994627cd7cfe
MB22 <matthieu.biache@gmail.com>
2013-02-11 08:07:22 -0800

    Create usart.c

---
+++ usart.c
@@ -0,0 +1,18 @@
+/*
+usart.c
+Crée par Matthieu Biache et Quentin Viotto le 11 février 2013
+Derniere modification: 11 février 2013
+*/
+
+#include "usart.h"
+
+//Initialisation et démarrage de l'USART
+
+void fct_init_USART(void)
+{
+}
+
+void fct_start_USART(void)
+{
+}
+
