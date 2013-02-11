60002e00b6e94ed3673ecc4cd631d30c660bf508
fayetcedric <compte@cfayet.eu>
2013-02-11 15:51:14 +0100

    Update constante.h

--- constante.h
+++ constante.h
@@ -8,6 +8,7 @@ Fichier contenant les différentes constantes pré-processeur
 #ifndef __CONSTANCE_H
 #define __CONSTANCE_H
 
-#define TIME_PERIOD_TIMERDUMAIN 5000 //ms
+#define CLK_TIMERDUMAIN TC_CLKS_32 //cadence
+#define TIME_PERIOD_TIMERDUMAIN 5000 //période
 
 #endif
