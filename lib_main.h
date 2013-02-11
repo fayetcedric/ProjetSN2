536e729b535906ec847086477b9bff26241d42df
fayetcedric <compte@cfayet.eu>
2013-02-11 17:20:09 +0100

    Update lib_main.h

--- lib_main.h
+++ lib_main.h
@@ -11,6 +11,11 @@ Derniere modification:
 #define LIB_MAIN_H
 
 void  fct_init_VariablesGlobales(); // Fonctions initialisant les variables globales
+
+void fct_init_AIC();
+
 void fct_init_TimerDuMain();
+void fct_start_TimerDuMain();
+void fct_irq_IncremCptTimer0();
 
 #endif
