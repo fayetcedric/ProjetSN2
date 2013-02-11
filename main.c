f2f8b6e6e28b4749f785a2f1cdd56bd0379ea2dd
fayetcedric <compte@cfayet.eu>
2013-02-11 14:24:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,6 @@
 
 #include "lib_main.h"
 
-/* Constantes globales : */
-int flag_DemandeUtilisateur = 0;
-int flag_DemandeTemperature = 0;
-int flag_DemandeVitesseVent = 0;
-int flag_DemandePluviometrie = 0;
-
-int value_Temperature; // Température est comprise entre -55° et 150°
-int value_VitesseVent;
-int value_Pluviometrie;
-
 void main(void)
 {
 	/* Initialisation des fonctions du main : */
