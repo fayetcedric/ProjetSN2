30ab7dd534072ba0572eaa7491cb481c5e8b2f25
fayetcedric <compte@cfayet.eu>
2013-02-11 14:58:34 +0100

    Update lib_main.h

--- lib_main.h
+++ lib_main.h
@@ -4,7 +4,12 @@ Crée par Cédric Fayet le 11 février 2013
 Derniere modification:
 */
 
+#include "constante.h"
+#include "global.h"
+
 #ifndef __LIB_MAIN_H
 #define LIB_MAIN_H
 
-void  fct_init_VariablesGlobales(); 
+void  fct_init_VariablesGlobales(); // Fonctions initialisant les variables globales
+
+#endif
